9b37a070ceda36ece8ce499f354788de2b91edc4
Marco <marcomenchetti96@gmail.com>
2023-05-17 10:38:31 +0200

    io_examples basics

--- io_examples/src/scanf_example.c
+++ io_examples/src/scanf_example.c
@@ -5,9 +5,14 @@ int main( int argc, char* argv[] ) {
     int age;
     printf("What is your name? ");
     scanf("%s",name);
+    // variable `name` does not require the address operator `&`
+    // because of the structure of arrays, its name already represent the 
+    // pointer to the first element:
+    printf("Hello, %s, the first letter of your name is \'%c\'\n", name, *name);
 
-    printf("Hello, %s, how old are you? ",name);
+    printf("How old are you? ");
     scanf("%d",&age);
+    // variable `age` requires the address operator
 
     printf("Wow, %d years are a lot!\n",age);
     return 0;
