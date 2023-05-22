2c1b293317f85837c68df37cca3bacfebf653b5a
Marco <marcomenchetti96@gmail.com>
2023-05-22 13:07:08 +0200

    Error case on switch statement

--- control_flow/src/switch.c
+++ control_flow/src/switch.c
@@ -33,6 +33,26 @@ int main( int argc, char* argv[] ) {
             printf("Well, now I do not know where you are going! What is %d?\n", goal);
             break;
     }
+
+    int numero = 3;
+    printf("Type an integer from 0 to 5: ");
+    scanf("%d", &numero);
+
+    switch( numero ) {
+        //forgetting the brake statement will result in unwanted behaviour
+        case 0:
+            printf("numero is 0\n");
+        case 1:
+            printf("numero is 1\n");
+        case 2:
+            printf("numero s between 0 e 2\n"); 
+            break;
+        case 5:
+            printf("numero is equal to 5\n");
+        default:
+            printf("Don't know what to do\n");
+            break;
+    }
     return 0;
 }
 
